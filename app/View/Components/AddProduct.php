6bd58e881b2645f4d418591de614015c30476e03
hendr1an <hendriansyahgyan@gmail.com>
2026-04-24 01:59:49 +0700

    menambahkan class component AddProduct untuk UI

--- app/View/Components/AddProduct.php
+++ app/View/Components/AddProduct.php
@@ -8,12 +8,16 @@ use Illuminate\View\Component;
 
 class AddProduct extends Component
 {
+    public string $url;
+    public string $name;
+
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct(string $url, string $name)
     {
-        //
+        $this->url = $url;
+        $this->name = $name;
     }
 
     /**
